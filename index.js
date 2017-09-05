804772efecf92330d4042b03d5ed2c0dd116962d
Adem Saglam <asaglam84@hotmail.com>
2017-09-05 18:40:31 +0300

    aasd

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('express')();
 
 app.get('/health-check', (req, res) => res.sendStatus(200));
+app.get('/check', (req, res) => {
+    res.send({test: ok});
+    //res.sendStatus(200);
+});
 
 app.listen(8080);
